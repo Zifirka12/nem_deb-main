1fd7dac9d992a4b68261491300f1df5fd0f82db9
Student <Ilaafanasef21@gmail.com>
2025-01-22 15:52:49 +0300

    игры готовы

--- index.js
+++ index.js
@@ -226,3 +226,69 @@ const obj = {
             
             
     }
+
+    //Дз №7
+    function turnOverText() {
+        let regexp = /^[a-zA-Zа-яА-Я]*$/;
+        let userEnter;
+    
+        do {
+            userEnter = prompt("Введите ваше слово!");
+            if (regexp.test(userEnter)) {
+                alert(userEnter.split('').reverse().join(''));
+            } else {
+                alert("Вы ввели не слово введите, пожалуйста, слово");
+            }
+        } while (regexp.test(userEnter) === false);
+    
+    }
+    
+    
+    
+    function simpleQuizGame() {
+        
+        let userAnswer;
+        let startMessage;
+        let pointCounter = 0;
+        let sum;
+        const quiz = [
+            {
+                question: "Какой цвет небо?",
+                options: ["1. Красный", " 2. Синий", " 3. Зеленый"],
+                correctAnswer: 2
+            },
+            {
+                question: "Сколько дней в неделе?",
+                options: ["1. Шесть", " 2. Семь", " 3. Восемь"],
+                correctAnswer: 2
+            },
+            {
+                question: "Сколько у человека пальцев на одной руке?",
+                options: ["1. Четыре", " 2. Пять", " 3. Шесть"],
+                correctAnswer: 2
+            }
+        ];
+    
+        do {
+        startMessage = confirm("Хотите поиграть в игру?");
+        if (startMessage === true) {
+                for (let i = 0; i < quiz.length; i++) {
+                    userAnswer = Number(prompt(`Ответьте на вопрос: ${quiz[i].question}, \nварианты ответа: \n${quiz[i].options} \n(введите номер ответа)`));
+                if (userAnswer === quiz[i].correctAnswer) {
+                    alert("Right");
+                    pointCounter++;
+                    sum = pointCounter;
+                    alert(`Скок навармил ${pointCounter} баллов`);
+                    
+                } else {
+                    alert("Wrong answer!");
+                }
+                }
+                alert(`Вы набрали: ${sum} балл(а). Ты крут!`);
+        } else {
+            alert("Прощай!");
+            break;
+        }
+    } while (true);
+        
+    }
